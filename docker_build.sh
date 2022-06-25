cd60f17fd80ef261fbf62f37b37405165c24e474
salenjhons <electromehanic@rambler.ru>
2022-06-25 12:56:30 +0300

    logging-1

--- docker_build.sh
+++ docker_build.sh
@@ -1,6 +1,12 @@
-#!/bin/bash
+!/bin/bash
 
-echo `git show --format="%h" HEAD | head -1` > build_info.txt
-echo `gti rev-parse --abbrev=ref HEAD` >> build_info.txt
+# echo `git show --format="%h" HEAD | head -1` > build_info.txt
+# echo `gti rev-parse --abbrev=ref HEAD` >> build_info.txt
 
-docker build -t $USER_NAME/ui
+# docker build -t $USER_NAME/ui
+
+export USER_NAME='johntelegin'
+
+cd ./src/ui && bash docker_build.sh && docker push $USER_NAME/ui
+cd ../post-py && bash docker_build.sh && docker push $USER_NAME/post
+cd ../comment && bash docker_build.sh && docker push $USER_NAME/comment
